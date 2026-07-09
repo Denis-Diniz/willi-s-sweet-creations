c8bb2f9070e8eaca68c087dd255e50b44e4ad059
Denis-Diniz <denishdiniz1@gmail.com>
2026-07-09 16:50:44 -0300

    Change favicon from SVG to JPEG format

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -96,7 +96,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+      { rel: "icon", type: "image/jpeg", href: "/Logo_favicon.jpeg" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
